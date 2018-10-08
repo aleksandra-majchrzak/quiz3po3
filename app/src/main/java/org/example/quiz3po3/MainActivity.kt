9053763a3154a823a6ccd2f748d6d6d33c887ae5
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2018-10-08 21:09:45 +0200

    Dummy button with Snackbar.

--- app/src/main/java/org/example/quiz3po3/MainActivity.kt
+++ app/src/main/java/org/example/quiz3po3/MainActivity.kt
@@ -1,12 +1,18 @@
 package org.example.quiz3po3
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener { _ ->
+            Snackbar.make(mainView, "Button clicked", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
