833f7b036835db47836da149c6dc0651cadd0be3
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2018-10-09 20:38:05 +0200

    Added dummy questions list.

--- app/src/main/java/org/example/quiz3po3/GameActivity.kt
+++ app/src/main/java/org/example/quiz3po3/GameActivity.kt
@@ -5,7 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View.*
 import kotlinx.android.synthetic.main.activity_game.*
-import org.example.quiz3po3.db.DbProvider
+import org.example.quiz3po3.db.Question
+import java.util.*
 
 class GameActivity : AppCompatActivity() {
 
@@ -16,7 +17,10 @@ class GameActivity : AppCompatActivity() {
         /*val questions = DbProvider.getDatabase(applicationContext).questionDao().selectAll()
         Log.e("GameActivity", questions.toString())*/
 
-        val question = DbProvider.getDatabase(applicationContext).questionDao().selectRandom(arrayOf())
+        val questions = listOf(Question(1, "LAMA W KOSMOSIE"), Question(2, "GŁODNA GODZILLA"), Question(3, "WŚCIEKŁA WIEWIÓRKA"), Question(4, "SPIDER-PIG"))
+        val question = questions.get(Math.abs(Random().nextInt()) % 4)
+
+        //val question = DbProvider.getDatabase(applicationContext).questionDao().selectRandom(arrayOf())
         Log.e("GameActivity", question?.toString() ?: "empty list")
         text.text = question?.text ?: ""
     }
